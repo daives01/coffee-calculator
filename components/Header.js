079ceeb3b86e4f7a2cbe023ab9f76227bfd30b64
Daniel Ives <danielives012@yahoo.com>
2022-01-01 23:36:07 -0700

    fix mobile view

--- components/Header.js
+++ components/Header.js
@@ -129,7 +129,7 @@ const DesktopSubNav = ({ label, href, subLabel }) => {
 
 const MobileNav = () => {
   return (
-    <Stack bg={useColorModeValue("white", "gray.800")} p={4} display={{ md: "none" }}>
+    <Stack bg={"creme"} p={4} display={{ md: "none" }}>
       {NAV_ITEMS.map((navItem) => (
         <MobileNavItem key={navItem.label} {...navItem} />
       ))}
@@ -145,25 +145,27 @@ const MobileNavItem = ({ label, children, href }) => {
       <Flex
         py={2}
         as={Link}
-        href={href ?? "/"}
+        href={href ?? null}
         justify={"space-between"}
         align={"center"}
         _hover={{
           textDecoration: "none",
         }}
       >
-        <Text fontWeight={600} color={useColorModeValue("gray.600", "gray.200")}>
+        <Text fontWeight={600} color={"darkBrown"}>
           {label}
         </Text>
         {children && <Icon as={ChevronDownIcon} transition={"all .25s ease-in-out"} transform={isOpen ? "rotate(180deg)" : ""} w={6} h={6} />}
       </Flex>
 
       <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
-        <Stack mt={2} pl={4} borderLeft={1} borderStyle={"solid"} borderColor={useColorModeValue("gray.200", "gray.700")} align={"start"}>
+        <Stack mt={2} pl={4} borderLeft={1} borderStyle={"solid"} borderColor={"darkBrown"} align={"start"}>
           {children &&
             children.map((child) => (
               <Link key={child.label} py={2} href={child.href}>
-                {child.label}{" "}
+                <a>
+                {child.label}
+                </a>
               </Link>
             ))}
         </Stack>
@@ -186,7 +188,6 @@ const NAV_ITEMS = [
   },
   {
     label: "Aeropress",
-    href:"/aeropress/aeropress-1",
     children: [
       {
         label: "My Go-To 2m30s Brew",
